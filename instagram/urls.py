4c07f8a06b588c9c9ffa61cf7b8bcc0462e4cf77
daniel njuguna <njugunadaniel364@gmail.com>
2019-10-17 13:00:56 +0300

    set the route that the registration form will be accessed by and the registration template

--- instagram/urls.py
+++ instagram/urls.py
@@ -15,13 +15,15 @@ Including another URLconf
 """
 from django.urls import path, include
 from django.contrib import admin
+from django.contrib.auth import views as auth_views 
+from users import views as user_views
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('register/', user_views.register, name='register'),
     path('',include('insta.urls'))
-    
 ]
 if settings.DEBUG:
         urlpatterns+=static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
