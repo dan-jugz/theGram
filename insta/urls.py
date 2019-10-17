985aa385939cf617da2500d0e04c9aaf0506aab3
daniel njuguna <njugunadaniel364@gmail.com>
2019-10-17 15:46:37 +0300

    add the views to the  authentication processes

--- insta/urls.py
+++ insta/urls.py
@@ -1,6 +1,5 @@
 from django.urls import path, include
-from .views import PostListView,PostCreateView,PostDetailView,PostDeleteView,PostUpdateView
-
+from .views import PostListView,PostCreateView,PostDetailView,PostDeleteView,PostUpdateView,saved_posts,PostLikeToggle
 app_name = 'insta'
 
 urlpatterns = [
@@ -8,5 +7,7 @@ urlpatterns = [
     path('new/', PostCreateView.as_view(),name='post_create'),
     path('<int:id>/', PostDetailView.as_view(), name='post_detail'),
     path('<int:id>/update/', PostUpdateView.as_view(), name='post_update'),
-    path('<int:id>/delete/', PostDeleteView.as_view(), name='post_delete')
+    path('<int:id>/delete/', PostDeleteView.as_view(), name='post_delete'),
+    path('<int:id>/likes/', PostLikeToggle.as_view(), name='like_toggle'),
+    path('saved/', saved_posts, name='saved_posts'),
 ]
