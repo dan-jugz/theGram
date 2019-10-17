98e0ef2ea6888590ff3d9738c2af6bb095a51e1b
daniel njuguna <njugunadaniel364@gmail.com>
2019-10-17 10:41:58 +0300

    set the configurations and images to the posts

--- instagram/settings.py
+++ instagram/settings.py
@@ -121,3 +121,16 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+)
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+# Directory to save our uploaded files
+MEDIA_ROOT=os.path.join(BASE_DIR,'media')
+MEDIA_URL='/media/'
+
+
+CRISPY_TEMPLATE_PACK='bootstrap4'
