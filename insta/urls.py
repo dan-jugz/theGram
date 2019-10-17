e31cf0d1c46e1113b8aa0dab6d455cd8c9c67e2c
daniel njuguna <njugunadaniel364@gmail.com>
2019-10-17 09:49:02 +0300

    define a route to be used by the update and delete view functions

--- insta/urls.py
+++ insta/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path, include
-from .views import PostListView,PostCreateView,PostDetailView,
+from .views import PostListView,PostCreateView,PostDetailView,PostDeleteView,PostUpdateView
 
 app_name = 'insta'
 
@@ -7,5 +7,6 @@ urlpatterns = [
     path('', PostListView.as_view(), name='post_list')
     path('new/', PostCreateView.as_view(),name='post_create')
     path('<int:id>/', PostDetailView.as_view(), name='post_detail'),
-    
+    path('<int:id>/update/', PostUpdateView.as_view(), name='post_update'),
+    path('<int:id>/delete/', PostDeleteView.as_view(), name='post_delete')
 ]
