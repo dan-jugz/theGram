9ed72edcb04196cfbf05d0b1ca1f27fea3b18452
daniel njuguna <njugunadaniel364@gmail.com>
2019-10-17 13:03:52 +0300

    set the corresponding route and template to display profile info

--- instagram/urls.py
+++ instagram/urls.py
@@ -23,6 +23,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('register/', user_views.register, name='register'),
+    path('profile/', user_views.profile, name='profile'),
     path('',include('insta.urls'))
 ]
 if settings.DEBUG:
