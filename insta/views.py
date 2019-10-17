39737eecdcddbfa97901e71694e045dd99a937f3
daniel njuguna <njugunadaniel364@gmail.com>
2019-10-17 09:36:56 +0300

    setup the class views to render homepage, and details and post creation template

---
+++ insta/views.py
@@ -0,0 +1,33 @@
+from django.shortcuts import render,get_object_or_404,redirect
+from django.urls import reverse 
+from datetime import datetime
+from django.utils import timezone
+from .models import Post 
+from .forms import PostForm 
+from django.views.generic import ListView,CreateView,DetailView,
+
+# Create your views here.
+class PostListView(ListView):
+    template_name = 'insta/home.html'
+    queryset = Post.objects.all().filter(created_date__lte=timezone.now()).order_by('-created_date')
+    context_object_name = 'posts'
+
+class PostCreateView(CreateView):
+    template_name ='insta/create.html'
+    form_class = PostForm
+    queryset = Post.objects.all()
+    success_url = '/'
+
+    def form_valid(self,form):
+        print(form.cleaned_data)
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+class PostDetailView(DetailView):
+    template_name = 'insta/details.html'
+    queryset = Post.objects.all().filter(created_date__lte=timezone.now())
+
+    def get_object(self):
+        id_ = self.kwargs.get('id')
+        return get_object_or_404(Post, id=id_)
+
