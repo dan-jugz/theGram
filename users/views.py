6c92cba5298dd88007be79e4431214be446510b3
daniel njuguna <njugunadaniel364@gmail.com>
2019-10-17 12:59:41 +0300

    define a register function that regisers a new user to the function

--- users/views.py
+++ users/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from .forms import UserRegisterForm
 
 # Create your views here.
+
+def register(request):
+    if request.method == 'POST':
+        print("This is working bro.")
+        form = UserRegisterForm(request.POST)
+        
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            print(username,"This is working")
+            messages.success(request, f'Account created for {username} successfully!')
+            return redirect('/')
+    else:
+        #If there is no a post request just send an empty form
+        form = UserRegisterForm()
+    return render(request, 'users/register.html', {'form': form})
+
+
+
+
